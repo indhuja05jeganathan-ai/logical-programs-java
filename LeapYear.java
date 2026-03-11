8f26bea1304806e466305275d40676a31cb91f0d
indhuja05jeganathan-ai <indhuja05jeganathan@gmail.com>
2026-03-12 00:12:47 +0530

    Add LeapYear class to check leap years

---
+++ LeapYear.java
@@ -0,0 +1,14 @@
+class leap
+{
+    public static void main(String[]args)
+    {
+        int year=2045;
+        if(year%4==0)
+        {
+            System.out.println("leap");
+        }
+        else{
+            System.out.println("not a leap year");
+        }
+    }
+}
