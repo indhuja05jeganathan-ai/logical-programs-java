8edd355466e84f34dd545e9e6729e53c6dd35759
indhuja05jeganathan-ai <indhuja05jeganathan@gmail.com>
2026-03-12 00:09:28 +0530

    Add DataType class with main method and variables

---
+++ DataType.java
@@ -0,0 +1,18 @@
+public class datatype
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+        byte c=33;
+        float f=56.4f;
+        double d=47.45799;
+        char h='j';
+       System.out.println(h);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        
+        
+    }
+}
