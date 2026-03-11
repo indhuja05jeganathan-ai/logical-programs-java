41efff3d18907de72b95a0a864b9f5a2325c20b4
indhuja05jeganathan-ai <indhuja05jeganathan@gmail.com>
2026-03-12 00:05:13 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+
+class fact
+{
+    public static void main(String[]args)
+    {
+        int fact=1,i,n=5;
+        for(i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
